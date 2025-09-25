d6d6b8e0c7820da82ffffb616b4aeaf3677cf331
NateTheUnavailable <nate@stevenogardner.com>
2025-09-25 10:00:22 -0600

    working

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ app.use('/', require('./routes'));
 app.listen(port, () =>{
   console.log(`Server is running on port ${port}`);
 });
+async function main() {
+	const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+}
